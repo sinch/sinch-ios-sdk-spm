8384e5543d2daac6de0eb09c70ee891ceb3f0c96
Vladyslav Zelinskyi <vladyslav.zelinskyi@sinch.com>
2025-09-29 11:06:54 +0200

    Added Package.swift and Readme

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+  name: "SinchRTC",
+  platforms: [.iOS(.v12)],
+  products: [
+    .library(name: "SinchRTC", targets: ["SinchRTC"])
+  ],
+  targets: [
+    .binaryTarget(
+      name: "SinchRTC",
+      url: "https://download.sinch.com/ios/temp/spm/SinchRTC-iOS-5.35.23%2b848c0d35.zip",
+      checksum: "1806df5c4d6eeaad8e7f3c400819cd57bc28b20bf1ab7e751f5cc99490b38778"
+    )
+  ]
+)
