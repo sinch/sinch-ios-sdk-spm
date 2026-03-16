79cd75c50f8873ad57e517ee3ff282f19a90a103
CI Pipeline <dev@sinch.com>
2026-03-16 12:05:37 +0100

    Update Package.swift to v5.38.7

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "SinchRTC",
-      url: "https://download.sinch.com/ios/5.37.4/apple/dynamic/SinchRTC-iOS-5.37.4%2b9f068e4d.zip",
-      checksum: "387a244a9d44887e6a2726b00af4981e6785312a75dbf641a2d142f4df67a292"
+      url: "https://download.sinch.com/ios/5.38.7/apple/dynamic/SinchRTC-iOS-5.38.7%2ba37f28ca.zip",
+      checksum: "7600c2261eb738de5dde3000b8fc9b20bf08edff4985f3ec5bb921f48632bea7"
     ),
     .target(
       name: "SinchRTCLinkedFrameworks",
